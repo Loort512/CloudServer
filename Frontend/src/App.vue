d2a5e24b93d79fd01b8ff20871b0528a7216ef26
ubuntu <ubuntu>
2022-07-06 22:45:44 +0200

    Frontend init - connected to backend

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav>
+  <nav class="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/login">Login</router-link><br>
   </nav>
@@ -18,19 +18,19 @@ export default{
   },
   created(){
     // create Test Data
-    this.$store.dispatch('addStudent',{id: '2', firstName: 'Max', lastName: 'Muster', dob: '10.10.1995', gender: 'm', department: 'HTW', emailID: 'max.muster@yahoo.de'} );
-    this.$store.dispatch('addStudent',{id: '3', firstName: 'Moni', lastName: 'Muster', dob: '10.11.1995', gender: 'm', department: 'HTW', emailID: 'moni.muster@yahoo.de'} );
-    this.$store.dispatch('addStudent',{id: '4', firstName: 'Franz', lastName: 'Muster', dob: '10.12.1995', gender: 'm', department: 'HTW', emailID: 'franz.muster@yahoo.de'} );
-    this.$store.dispatch('addStudent',{id: '5', firstName: 'Tom', lastName: 'Muster', dob: '10.10.1997', gender: 'm', department: 'HTW', emailID: 'tom.muster@yahoo.de'} );
-    this.$store.dispatch('addStudent',{id: '6', firstName: 'Henrike', lastName: 'Muster', dob: '10.10.1999', gender: 'w', department: 'HTW', emailID: 'he.muster@yahoo.de'} );
-    this.$store.dispatch('addStudent',{id: '7', firstName: 'Laura', lastName: 'Muster', dob: '10.10.1990', gender: 'w', department: 'HTW', emailID: 'laura.muster@yahoo.de'} );
+    // this.$store.dispatch('addStudent',{id: '2', firstName: 'Max', lastName: 'Muster', dob: '10.10.1995', gender: 'm', department: 'HTW', emailID: 'max.muster@yahoo.de'} );
+    // this.$store.dispatch('addStudent',{id: '3', firstName: 'Moni', lastName: 'Muster', dob: '10.11.1995', gender: 'm', department: 'HTW', emailID: 'moni.muster@yahoo.de'} );
+    // this.$store.dispatch('addStudent',{id: '4', firstName: 'Franz', lastName: 'Muster', dob: '10.12.1995', gender: 'm', department: 'HTW', emailID: 'franz.muster@yahoo.de'} );
+    // this.$store.dispatch('addStudent',{id: '5', firstName: 'Tom', lastName: 'Muster', dob: '10.10.1997', gender: 'm', department: 'HTW', emailID: 'tom.muster@yahoo.de'} );
+    // this.$store.dispatch('addStudent',{id: '6', firstName: 'Henrike', lastName: 'Muster', dob: '10.10.1999', gender: 'w', department: 'HTW', emailID: 'he.muster@yahoo.de'} );
+    // this.$store.dispatch('addStudent',{id: '7', firstName: 'Laura', lastName: 'Muster', dob: '10.10.1990', gender: 'w', department: 'HTW', emailID: 'laura.muster@yahoo.de'} );
 
-    this.$store.dispatch('addStaff',{id: '2', firstName: 'Max1', lastName: 'Muster', dob: '10.10.1989', gender: 'm', department: 'HTW', emailID: 'max.muster@yahoo.de'} );
-    this.$store.dispatch('addStaff',{id: '3', firstName: 'Moni1', lastName: 'Muster', dob: '09.05.1995', gender: 'm', department: 'HTW', emailID: 'moni.muster@yahoo.de'} );
-    this.$store.dispatch('addStaff',{id: '4', firstName: 'Franz1', lastName: 'Muster', dob: '11.04.1995', gender: 'm', department: 'HTW', emailID: 'franz.muster@yahoo.de'} );
-    this.$store.dispatch('addStaff',{id: '5', firstName: 'Tom1', lastName: 'Muster', dob: '10.06.1995', gender: 'm', department: 'HTW', emailID: 'tom.muster@yahoo.de'} );
-    this.$store.dispatch('addStaff',{id: '6', firstName: 'Henrike1', lastName: 'Muster', dob: '10.08.1995', gender: 'w', department: 'HTW', emailID: 'he.muster@yahoo.de'} );
-    this.$store.dispatch('addStaff',{id: '7', firstName: 'Laura1', lastName: 'Muster', dob: '10.10.1995', gender: 'w', department: 'HTW', emailID: 'laura.muster@yahoo.de'} );
+    // this.$store.dispatch('addStaff',{id: '2', firstName: 'Max1', lastName: 'Muster', dob: '10.10.1989', gender: 'm', department: 'HTW', emailID: 'max.muster@yahoo.de'} );
+    // this.$store.dispatch('addStaff',{id: '3', firstName: 'Moni1', lastName: 'Muster', dob: '09.05.1995', gender: 'm', department: 'HTW', emailID: 'moni.muster@yahoo.de'} );
+    // this.$store.dispatch('addStaff',{id: '4', firstName: 'Franz1', lastName: 'Muster', dob: '11.04.1995', gender: 'm', department: 'HTW', emailID: 'franz.muster@yahoo.de'} );
+    // this.$store.dispatch('addStaff',{id: '5', firstName: 'Tom1', lastName: 'Muster', dob: '10.06.1995', gender: 'm', department: 'HTW', emailID: 'tom.muster@yahoo.de'} );
+    // this.$store.dispatch('addStaff',{id: '6', firstName: 'Henrike1', lastName: 'Muster', dob: '10.08.1995', gender: 'w', department: 'HTW', emailID: 'he.muster@yahoo.de'} );
+    // this.$store.dispatch('addStaff',{id: '7', firstName: 'Laura1', lastName: 'Muster', dob: '10.10.1995', gender: 'w', department: 'HTW', emailID: 'laura.muster@yahoo.de'} );
 
   }  
 
@@ -47,7 +47,8 @@ export default{
 }
 
 nav {
-  padding: 30px;
+  padding: 5%;
+  background: #3c8733e8;
 }
 
 nav a {
@@ -66,18 +67,6 @@ body{
   background-size: cover;
 
 }  
-
-.content{
-    display: flex;
-    flex-direction: column;
-    height: auto;
-    min-height: 500px;
-    justify-content: center;
-    align-items: center;
-    background-color: #828282;
-    margin-left: 24%;
-    width: 53%;
-} 
 
 input[type=button]{
   height: 20pt;
--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -1,28 +1,37 @@
 <template>
   <div class="home">
     <div class="header">
-      <router-link v-if="this.$store.state.loggedInAs === 'admin'" to="/student" >Student</router-link> |  
+      <!-- <router-link v-if="this.$store.state.loggedInAs === 'admin'" to="/student" >Student</router-link> |  
       <router-link v-if="this.$store.state.loggedInAs === 'admin'" to="/staff" >Staff</router-link><br>
-      <router-link v-if="this.$store.state.loggedInAs === 'staff'" to="/courses" >Courses</router-link>
+      <router-link v-if="this.$store.state.loggedInAs === 'staff'" to="/courses" >Courses</router-link> -->
     </div>
-    <img alt="Vue logo" src="../assets/background.jpeg">
-    <HelloWorld msg="Welcome to your Management System"/>
+    <Content headerTitle="Welcome to your Private Cloud">
+      <template v-slot:content>
+        <img alt="Vue logo" src="../assets/privacy-background-image.jpeg">
+        <p>
+          Store your data in a personal and private Cloud 
+          <br><br><br>
+          Start by logging in 
+        </p>
+        <input type="button" value="Login">
+      </template>
+    </Content>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Content from '@/components/Content.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    Content
   },
   created(){
-    console.log("created")
-    this.$store.state.loggedInAs === "admin"
-    console.log("login as: ", this.$store.state.loggedInAs )
+    
   } 
 }
 </script>
